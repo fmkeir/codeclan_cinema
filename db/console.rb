e6487361438bb40888bc3a4b912741ea4c0cf669
FKeir <fmkeir@gmail.com>
2020-01-31 13:22:46 +0000

    Create ticket model with CRUD functionality

--- db/console.rb
+++ db/console.rb
@@ -1,8 +1,11 @@
 require('pry')
 require_relative('../models/customer')
 require_relative('../models/film')
+require_relative('../models/ticket')
 
 Customer.delete_all()
+Film.delete_all()
+Ticket.delete_all()
 
 customer1 = Customer.new({
   "name" => "Neil",
@@ -27,6 +30,24 @@ film2 = Film.new({
   "price" => 5
   })
 film2.save
+
+ticket1 = Ticket.new({
+  "customer_id" => customer1.id,
+  "film_id" => film1.id
+  })
+ticket1.save()
+
+ticket2 = Ticket.new({
+  "customer_id" => customer1.id,
+  "film_id" => film2.id
+  })
+ticket2.save()
+
+ticket3 = Ticket.new({
+  "customer_id" => customer2.id,
+  "film_id" => film2.id
+  })
+ticket3.save()
 
 binding.pry
 nil

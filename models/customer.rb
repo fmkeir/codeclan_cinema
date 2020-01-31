76bfe8adbd5b16043adf3ffc6c8ccd8b8de17d57
FKeir <fmkeir@gmail.com>
2020-01-31 13:33:19 +0000

    Enable films/customers to find their customers/films

--- models/customer.rb
+++ models/customer.rb
@@ -36,6 +36,16 @@ class Customer
     SqlRunner.run(sql, values)
   end
 
+  def films()
+    sql = "SELECT films.* FROM films
+    INNER JOIN tickets
+    ON films.id = tickets.film_id
+    WHERE customer_id = $1"
+    values = [@id]
+    films = SqlRunner.run(sql, values)
+    return films.map {|film| Film.new(film)}
+  end
+
   def self.all
     sql = "SELECT * FROM customers"
     customers = SqlRunner.run(sql)
--- models/film.rb
+++ models/film.rb
@@ -36,6 +36,16 @@ class Film
     SqlRunner.run(sql, values)
   end
 
+  def customers()
+    sql = "SELECT customers.* FROM customers
+    INNER JOIN tickets
+    ON customers.id = tickets.customer_id
+    WHERE film_id = $1"
+    values = [@id]
+    customers = SqlRunner.run(sql, values)
+    return customers.map {|customer| Customer.new(customer)}
+  end
+
   def self.all
     sql = "SELECT * FROM films"
     films = SqlRunner.run(sql)

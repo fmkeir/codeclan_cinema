e16dd13543c26b41fe4bd9e442b7f519fd5c1592
FKeir <fmkeir@gmail.com>
2020-01-31 12:59:18 +0000

    Add update, delete, all to customer

--- models/customer.rb
+++ models/customer.rb
@@ -21,6 +21,27 @@ class Customer
     @id = SqlRunner.run(sql, values)[0]["id"].to_i
   end
 
+  def update()
+    sql = "UPDATE customers SET
+    (name, funds)
+    = ($1, $2)
+    WHERE id = $3"
+    values = [@name, @funds, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM customers WHERE id = $1"
+    values =[@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.all
+    sql = "SELECT * FROM customers"
+    customers = SqlRunner.run(sql)
+    return customers.map {|customer| Customer.new(customer)}
+  end
+
   def self.delete_all
     sql = "DELETE FROM customers"
     SqlRunner.run(sql)
